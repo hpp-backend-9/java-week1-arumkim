d1bc463b69780778d8689d6658729f4280ceb933
Kim Arum(kiki) <kimrumm@naver.com>
2024-09-27 02:48:46 +0900

    comment:  exception 내용 변경

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -13,7 +13,7 @@ public record UserPoint(
     // 포인트 충전
     public UserPoint charge(long amount) {
         if (amount < 0) {
-            throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
+            throw new IllegalArgumentException("충전 포인트 0보다 커야 합니다.");
         }
         return new UserPoint(this.id, this.point + amount, System.currentTimeMillis());
     }
@@ -21,7 +21,7 @@ public record UserPoint(
     // 포인트를 사용
     public UserPoint use(long amount) {
         if (amount < 0) {
-            throw new IllegalArgumentException("사용 금액은 0보다 커야 합니다.");
+            throw new IllegalArgumentException("사용 포인트는 0보다 커야 합니다.");
         }
         if (this.point < amount) {
             throw new IllegalArgumentException("포인트가 부족합니다.");
